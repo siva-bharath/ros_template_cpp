51b23c952aa0cf4e088622b5f8b8ca6a853186b8
Siva Bharath <72798827+siva-bharath@users.noreply.github.com>
2021-05-05 07:59:03 +0100

    Add files via upload

---
+++ lesson_a/src/add_two_ints_client.cpp
@@ -0,0 +1,40 @@
+#include "ros/ros.h"
+#include "lesson_a/AddTwoInts.h"
+
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "add_two_ints_client");
+
+  if(argc != 3)
+  {
+
+    ROS_INFO("usage: add_two_ints_client X Y");
+    return 1;
+  }
+
+  ros::NodeHandle n;
+
+  ros::ServiceClient client = n.serviceClient<lesson_a::AddTwoInts>("add_two_ints");
+
+  lesson_a::AddTwoInts srv;
+
+  srv.request.a = atoll(argv[1]);
+  srv.request.b = atoll(argv[2]);
+
+  if(client.call(srv))
+  {
+
+    ROS_INFO("Sum: %ld", (long int)srv.response.sum);
+  }
+  else 
+  {
+    ROS_ERROR("Failed to call service add_two_ints");
+    return 1;
+  }
+
+  return 0;
+
+}
